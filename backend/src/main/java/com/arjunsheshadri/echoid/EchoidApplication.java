2b9609e22bbcc598f25d7e9c113e5271e8d6a7aa
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2025-05-08 20:27:05 -0400

    Added Spring Project in Backend

---
+++ backend/src/main/java/com/arjunsheshadri/echoid/EchoidApplication.java
@@ -0,0 +1,11 @@
+package com.arjunsheshadri.echoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoidApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EchoidApplication.class, args);
+  }
+}
---
+++ backend/src/test/java/com/arjunsheshadri/echoid/EchoidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjunsheshadri.echoid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
